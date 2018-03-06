f159d7745f8288535d9afa419f88925fc6b3f0e1
Jeffrey Paul <sneak@acidhou.se>
2018-03-06 18:15:51 -0500

    Update cloud-init.sh

--- provision.workbox/cloud-init.sh
+++ provision.workbox/cloud-init.sh
@@ -6,6 +6,19 @@
 #runcmd:
 #  - curl -fsSL https://raw.githubusercontent.com/sneak/hacks/master/provision.workbox/cloud-init.sh | bash
 
+# make sure we don't OOM
+if [[ ! -e /var/swapfile ]]; then
+    fallocate -l 2G /var/swapfile
+    chmod 600 /var/swapfile
+    mkswap /var/swapfile
+    echo '/var/swapfile none swap sw 0 0' | tee -a /etc/fstab
+fi
+swapon /var/swapfile
+
+echo 'vm.swappiness=80' >> /etc/sysctl.conf
+echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
+sysctl -p
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y install git
