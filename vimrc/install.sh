a133f27de4763e2bb05dce3d1632e8db3715fc46
Jeffrey Paul <jp@eeqj.com>
2018-03-06 17:28:07 -0500

    modified:   vimrc/install.sh

--- vimrc/install.sh
+++ vimrc/install.sh
@@ -4,6 +4,4 @@ mkdir -p ~/.vim/swap
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/undo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [[ ! -e ~/.vimrc ]]; then
-    cat "$DIR/vimrc" > ~/.vimrc
-fi
+cat "$DIR/vimrc" > ~/.vimrc
