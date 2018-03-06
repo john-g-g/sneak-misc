6776edd7a93343b3fee0155e37f40608bf0a51d2
Jeffrey Paul <jp@eeqj.com>
2018-03-06 16:59:28 -0500

    updated termux setup

--- termux/setup.sh
+++ termux/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 PKGS="
     bash-completion
     byobu
+    git
     mosh
     vim
 "
@@ -10,3 +10,7 @@ PKGS="
 apt update
 apt -y upgrade
 apt -y install $PKGS
+apt -y autoremove
+
+git config --global user.email jp@eeqj.com
+git config --global user.name "Jeffrey Paul"
