0c305125ec68eee70adf6e6ccd88fb5ac94735eb
Jeffrey Paul <sneak@acidhou.se>
2018-03-06 17:21:39 -0500

    Update cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -49,6 +49,10 @@ fi
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+nvm install node
+
 mkdir -p ~/.local/bashrc.d
 mkdir -p ~/.local/profile.d
 
