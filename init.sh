35e4c5443a1512779a7993d0c7daafbbfda6add7
Jeffrey Paul <sneak@acidhou.se>
2018-03-06 16:44:43 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get -y upgrade
-apt-get -y install vim byobu screen build-essential git mosh
+apt-get -y install vim byobu screen build-essential git mosh bash-completion
 
 if [[ ! -d /home/sneak ]]; then
     useradd -m sneak
