f475373462a944ed6ce4d6949e32295113bce53c
Jeffrey Paul <jp@eeqj.com>
2018-10-01 22:47:10 -0700

    bionic defaults

---
+++ bionic-defaults/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+apt update
+apt -y install lsb-release
+
+PKGS="
+    apt-transport-https
+    byobu
+    command-not-found
+    cryptsetup-bin
+    iptraf-ng
+    jq
+    less
+    lsof
+    runit
+    runit-systemd
+    daemontools
+    vim
+    wget
+    zfsutils-linux
+    zfs-auto-snapshot
+"
+
+MRUM="main restricted universe multiverse"
+URL="mirror://mirrors.ubuntu.com/mirrors.txt"
+
+cat > /etc/apt/sources.list <<__EOF__
+deb $URL $(lsb_release -cs)             $MRUM
+deb $URL $(lsb_release -cs)-updates     $MRUM
+deb $URL $(lsb_release -cs)-backports   $MRUM
+deb $URL $(lsb_release -cs)-security    $MRUM
+__EOF__
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt -y install $PKGS
+
+rm -rf /var/lib/apt/lists/*
