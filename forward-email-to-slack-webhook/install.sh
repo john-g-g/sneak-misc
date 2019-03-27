67fc2168c2f761cbd22a9ccffdf98c8f400e7b7a
Jeffrey Paul <jp@eeqj.com>
2019-03-27 09:08:58 -0700

    multi arg command in .forward needs quotes

--- forward-email-to-slack-webhook/install.sh
+++ forward-email-to-slack-webhook/install.sh
@@ -7,6 +7,6 @@ chmod +x /usr/local/bin/email_to_webhook
 
 mkdir -p /etc/environment.d
 
-echo "|/usr/bin/envdir /etc/environment.d /usr/local/bin/email_to_webhook" > /root/.forward
+echo "|\"/usr/bin/envdir /etc/environment.d /usr/local/bin/email_to_webhook\"" > /root/.forward
 
 echo "now put your webhook url in /etc/environment.d/SLACK_WEBHOOK_URL"
